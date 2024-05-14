0d8ca1aa6a8f24cdaa93a4f3a15207139d617b13
Mussie <musdzion@gmail.com>
2024-05-14 06:11:25 -0400

    edit some entries

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) DEFAULT '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
+  password VARCHAR(255) NOT NULL
 );
 
 --create table properties
