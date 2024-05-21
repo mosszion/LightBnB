14dcdab2a6de79bc24e03aa65636787e90c71832
Mussie <musdzion@gmail.com>
2024-05-21 02:49:14 -0400

    rename folder App

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -48,12 +48,13 @@ const getUserWithEmail = (email) => {
     });   
   };
   
-  
+///////////////////////////////////////////////////////////////////////////////////////
   /**
    * Get a single user from the database given their id.
    * @param {string} id The id of the user.
    * @return {Promise<{}>} A promise to the user.
   */
+ ///////////////////////////////////////////////////////////////////////////////////////
  const getUserWithId = function (id) {
   return pool
   .query('SELECT * FROM users WHERE email = $1', [id])
@@ -76,12 +77,13 @@ const getUserWithEmail = (email) => {
   });   
 };
 
-  
+ /////////////////////////////////////////////////////////////////////////////////////// 
   /**
    * Add a new user to the database.
    * @param {{name: string, password: string, email: string}} user
    * @return {Promise<{}>} A promise to the user.
   */
+ ///////////////////////////////////////////////////////////////////////////////////////
  const addUser = function (user) {
    const userId = Object.keys(users).length + 1;
    user.id = userId;
