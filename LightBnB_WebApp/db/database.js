4d348badf992e43f8710d3cec134d4c669ffd70d
Mussie <musdzion@gmail.com>
2024-05-21 09:56:34 -0400

    add query using WHERE CLAUSES

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -130,21 +130,40 @@ const getAllReservations = (guest_id, limit) =>  {
 //  */
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const getAllProperties = (options, limit) => {
-  
-  return pool
-  .query( `SELECT * FROM properties LIMIT $1`, [limit])
-  .then ((result) => {
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
-    return result.rows;
-  })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  } else if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `WHERE owner_id = $${queryParams.length}`;
+  }
 
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
 
+  // 5
+  console.log(queryString, queryParams);
 
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 
 ///////////////////////////////////////////////////////////////////////////////////////
 /**
