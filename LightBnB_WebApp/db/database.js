01f657ef87b625da5d49a194c19a9c900866e09b
Mussie <musdzion@gmail.com>
2024-05-21 05:11:10 -0400

    add additonal modifications to getAllReservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -99,16 +99,16 @@ const getUserWithEmail = (email) => {
   */
  ///////////////////////////////////////////////////////////////////////////////////////
 
-const getAllReservations = (guest_id, limit = 10) =>  {
+const getAllReservations = (guest_id, limit) =>  {
   return pool 
       .query (
     `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
       FROM reservations
       JOIN properties ON reservations.property_id = properties.id
       JOIN property_reviews ON properties.id = property_reviews.property_id
-      WHERE reservations.guest_id = $1
+      WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
       GROUP BY properties.id, reservations.id
-      ORDER BY reservations.start_date
+      ORDER BY reservations.start_date DESC
       LIMIT $2;
       `, [guest_id, limit])
       .then( (result) => {
