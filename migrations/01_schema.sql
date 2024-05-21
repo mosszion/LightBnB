1aab975423db2ebe4c2d9caace70c2e84cafca13
Mussie <musdzion@gmail.com>
2024-05-21 04:23:37 -0400

    update the app

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -2,6 +2,9 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS rates;
+DROP TABLE IF EXISTS guest_reviews;
+
 
 --create table users
 CREATE TABLE users (
@@ -53,7 +56,27 @@ CREATE TABLE property_reviews (
   message TEXT 
 );
 
+-- create table for rates
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
 
+)
+
+-- CREATE TABLE guest_reviews
+CREATE TABLE  guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+
+
+)
 
 
 
