1b7156ccd16570fcec4257acf0c93a0077d80ae6
Mussie <musdzion@gmail.com>
2024-05-21 10:20:01 -0400

    add query for range of price

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -140,13 +140,22 @@ const getAllProperties = function (options, limit = 10) {
   JOIN property_reviews ON properties.id = property_id
   `;
 
+  queryString += `WHERE 1+1`;
+
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
-  } else if (options.owner_id && options.city) {
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+   if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
-    queryString += `WHERE city LIKE $${queryParams.length} AND owner_id = $${queryParams.length}`;
+    queryString += `AND owner_id = $${queryParams.length}`;
+  }
+  if (options.minimum_price_per_night && options.maximum_price_per_night){
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND BETWEEN minimum_price_per_night = $${queryParams.length-1} AND maximum_price_per_night = $${queryParams.length}`
+
   }
 
   // 4
